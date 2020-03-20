b267f2d300d466827da844db1b0ffd58acf4275c
Slava Radune <slava.radune@gmail.com>
2020-03-20 13:11:44 +0200

    adding standing details

--- routes/standings.js
+++ routes/standings.js
@@ -4,6 +4,25 @@ var router = express.Router();
 
 const creds = require('../client_secret');
 
+function getDate(title) {
+    return title.split(' ')[2];
+}
+
+function collect_details(r) {
+    let details = [];
+    for (let i = 8; i < Object.keys(r).length; i = i + 2){
+        let key = Object.keys(r)[i];
+        let next_key = Object.keys(r)[i + 1];
+        let date = getDate(key);
+        let exercise = r[key];
+        let challenge = r[next_key];
+        details.push({'date' : date,
+                        'exercise': exercise != null && !isNaN(exercise) && exercise !== '' ? exercise : "--",
+                        'challenge': challenge != null && !isNaN(challenge) && challenge !== '' ? challenge : "--"})
+    }
+    return details;
+}
+
 async function accessSpreadSheet() {
     const doc = new GoogleSpreadsheet('1bV6nmiP0r2PD3jQNg1WqXT2cBjI6uBDVZz-plSRkO_g');
     await doc.useServiceAccountAuth(creds);
@@ -16,6 +35,7 @@ async function accessSpreadSheet() {
         kyu: r['חגורה'],
         city: r['עיר'],
         total_points: r['ניקוד מצטבר'],
+        standing_details: collect_details(r)
     })) || [];
 
 }
